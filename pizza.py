98b153388d06839282baa292698980fb0e0fdb4c
BenjaminKlenner <38057075+BenjaminKlenner@users.noreply.github.com>
2020-05-29 11:03:08 +1200

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,11 +1,14 @@
 def pizzapicker():
-    pizza_total = int(input("How many pizzas do you want? "))
-    pizzamaker(pizza_total)
-
-def pizzamaker(pizza_total,custom_cost):
+    pizza_total = 0
     order = []
-    while pizza_total != 0:
-        pizza = int(input("What pizza flavour do you want?\n1. Pepperoni\n2. Hawaiian\n3. Cheese\n"))
+    custom_pizzas = []
+    pizza_total = int(input("How many pizzas do you want? "))
+    custom_cost = 0
+    pizzamaker(pizza_total,custom_cost,order,custom_pizzas)
+
+def pizzamaker(pizza_total,custom_cost,order,custom_pizzas):
+    while pizza_total > 0:
+        pizza = int(input("What pizza flavour do you want?\n1. Pepperoni\n2. Hawaiian\n3. Cheese\n4. Custom\n"))
         if pizza == 1:
             order.append(1)
             pizza_total -= 1
@@ -16,19 +19,31 @@ def pizzamaker(pizza_total,custom_cost):
             order.append(3)
             pizza_total -= 1
         elif pizza == 4:
-            order.append(4)
-            custom_pizza(order)
             pizza_total -= 1
+            custom_pizza(pizza_total,custom_cost,order,custom_pizzas)
         else:
             while int(pizza) <= 0 and int(pizza) >= 4:
                 pizza = int(input("What pizza flavour do you want?\n1. Pepperoni\n2. Hawaiian\n3. Cheese\n"))
 
-        calculator(order)
+    calculator(order,custom_cost)
 
-def custom_pizza(order):
-    print("Make custom pizza")
-    custom_cost = 10
-    pizzamaker(custom_cost,order)
+def custom_pizza(pizza_total,custom_cost,order,custom_pizzas):
+    custom_pizzas.append(len(custom_pizzas) + 1)
+    print(custom_pizzas)
+    crust = input("What crust do you want?\n1. Thin\n2. Thick\n3. Stuffed")
+    if crust == 1:
+        custom_pizzas[len(custom_pizzas)] = 1
+    elif crust == 2:
+        custom_pizzas[len(custom_pizzas)] = 2
+    elif crust == 3:
+        custom_pizzas[len(custom_pizzas)] = 3
+    else:
+        while int(crust) <= 0 and int(crust) >= 3:
+            crust = input("What crust do you want?\n1. Thin\n2. Thick\n3. Stuffed")
+
+        
+    custom_cost += 10
+    pizzamaker(pizza_total,custom_cost,order,custom_pizzas)
             
     
 
@@ -41,7 +56,7 @@ def calculator(order,custom_cost):
 
     
     i = len(order)
-    pizza_cost = 0
+    pizza_cost = custom_cost
 
     while i > 0:
         if int(order[i - 1]) == 1:
@@ -52,6 +67,8 @@ def calculator(order,custom_cost):
             pizza_cost += p_3
         i -= 1
 
-    print(pizza_cost)
-
+    print("{}".format(pizza_cost))
+    hold = input("Press enter to restart")
+    pizzapicker()
+    
 pizzapicker()
